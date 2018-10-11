71a1d5a8a93b4f4c329f2413c0667030d8ece0d4
AK <karkoukli.anas@gmail.com>
2018-10-11 14:57:09 -0300

    seed-data now generates 10 restaurants, each with valid Buenos Aires address and 5 - 9 meals.

--- src/seedData/sample.ts
+++ src/seedData/sample.ts
@@ -4,6 +4,7 @@ import { Restaurant } from '../entity/restaurant';
 import { Customer } from '../entity/customer';
 import { Address } from '../entity/address';
 import * as dotenv from 'dotenv';
+import { Rating } from '../entity/rating';
 
 dotenv.config();
 
@@ -24,6 +25,24 @@ loadTypeOrmOptions()
 
         // drop and recreate schema
         await c.synchronize(true);
+
+        const addresses = [
+            'Tucumán 3424, CABA, Argentina',
+            'perú 600, CABA, Argentina',
+            'arenales 2302, CABA ,Argentina',
+            'Armenia 1641, CABA',
+            'Av. del Libertador 1902, Buenos Aires',
+            'Las Casas 4095, CABA',
+            '1549, Av. Carabobo, Flores, Buenos Aires',
+            'Olavarría 1601, CABA',
+            'Libertad 431, CABA',
+            'Av. Federico Lacroze 3779, CABA',
+        ];
+
+        // tslint:disable-next-line:max-line-length
+        const descriptionLorem = 'Duis eu sapien sodales, gravida sapien ac, maximus orci. Curabitur sollicitudin turpis diam, nec tincidunt nulla elementum auctor. In a arcu sed dolor porttitor egestas in placerat tortor. Nunc vehicula ipsum in lorem posuere vulputate. Morbi turpis duis';
+
+        const ratingGenerator = () => Math.floor(Math.random() * 10) + 1;
 
         // seed users
         const customer1 = new Customer();
@@ -49,9 +68,9 @@ loadTypeOrmOptions()
         await getRepository(Customer).save(customer2);
 
         // seed restaurants
-        const address3 = new Address();
-        address3.normalized = 'Tucumán 3424, CABA, Argentina';
         const owner1 = new Customer();
+        const address3 = new Address();
+        address3.normalized = 'Av. Díaz Vélez 5044, CABA';
         owner1.address = address3;
         owner1.canCreateRestaurant = true;
         owner1.userName = 'mrBigShot';
@@ -60,38 +79,33 @@ loadTypeOrmOptions()
         owner1.phone = '3333333333';
         await getRepository(Customer).save(owner1);
 
-        const restaurant1 = new Restaurant();
-        const address4 = new Address();
-        address4.normalized = 'perú 600, CABA, Argentina';
-        restaurant1.address = address4;
-        restaurant1.email = 'great.eats@email.com';
-        restaurant1.name = 'great eats';
-        restaurant1.owner = owner1;
-        const burger = new Meal();
-        burger.name = 'house burger';
-        burger.description = 'biggest burger around';
-        burger.price = 150;
-        const stew = new Meal();
-        stew.name = 'winter stew';
-        stew.description = 'hearty and delicious';
-        stew.price = 240;
-        restaurant1.meals = [burger, stew];
-        await getRepository(Restaurant).save(restaurant1);
-
-        const restaurant2 = new Restaurant();
-        const address5 = new Address();
-        address5.normalized = 'arenales 2302, CABA';
-        restaurant2.name = 'fancy eats';
-        restaurant2.owner = owner1;
-        restaurant2.email = 'fancy.eats@email.com';
-        restaurant2.address = address5;
-        const pizza = new Meal();
-        pizza.price = 350;
-        pizza.name = 'deep dish pizza';
-        pizza.description = 'delicious and greasy';
-        restaurant2.meals = [pizza];
-        await getRepository(Restaurant).save(restaurant2);
-
+        // addresses.forEach(async (normalizedAddress, index) => {
+        for (let i = 0; i < addresses.length; i++) {
+            const restaurantName = `resto - ${i}`;
+            const restaurant = new Restaurant();
+            const address = new Address();
+            address.normalized = addresses[i];
+            restaurant.address = address;
+            restaurant.email = `${restaurantName}@email.com`;
+            restaurant.name = restaurantName;
+            restaurant.owner = owner1;
+            const meals = [];
+            for (let j = 0; j <= Math.floor(Math.random() * 5) + 5; j++) {
+                const meal = new Meal();
+                meal.name = `Meal - ${i} - ${j}`;
+                meal.description = descriptionLorem.substr(0, Math.floor(Math.random() * 155) + 100);
+                meal.price = Math.random() * 1000 + 200;
+                meals.push(meal);
+            }
+            restaurant.meals = meals;
+            await getRepository(Restaurant).save(restaurant);
+            const savedRestaurant = await getRepository(Restaurant).findOne({name: restaurantName});
+            const rating = new Rating();
+            rating.customer = customer1;
+            rating.score = ratingGenerator();
+            rating.restaurant = savedRestaurant!;
+            await getRepository(Rating).save(rating);
+        }
     })
     // tslint:disable-next-line:no-console
     .then(_ => console.log('seed data loaded!'))
